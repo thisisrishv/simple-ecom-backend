20f9b20be432d3407b9623681373917c8f375011
Rishabh Verma <thisisrishv@gmail.com>
2026-03-20 12:56:11 +0530

    functionality for fetching product image by id

--- src/main/java/com/rishv/ecom/proj/controller/ProductController.java
+++ src/main/java/com/rishv/ecom/proj/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.rishv.ecom.proj.model.Product;
 import com.rishv.ecom.proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,5 +50,16 @@ public class ProductController {
         catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = service.getProductById(productId);
+
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
     }
 }
