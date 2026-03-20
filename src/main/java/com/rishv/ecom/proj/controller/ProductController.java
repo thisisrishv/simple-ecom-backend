f1e9b7a76035e7aa79ef32f11c15a2cb7510da05
Rishabh Verma <thisisrishv@gmail.com>
2026-03-20 13:37:08 +0530

    functionality for updating and deleting the product

--- src/main/java/com/rishv/ecom/proj/controller/ProductController.java
+++ src/main/java/com/rishv/ecom/proj/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -19,11 +20,6 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-
-    @RequestMapping("/")
-    public String greet(){
-        return "Hi there";
-    }
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getAllProducts(){
@@ -61,5 +57,36 @@ public class ProductController {
         return ResponseEntity.ok()
                 .contentType(MediaType.valueOf(product.getImageType()))
                 .body(imageFile);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile){
+
+        Product product1 = null;
+        try {
+            product1 = service.updateProduct(id, product, imageFile);
+        }
+        catch (IOException e){
+            return new ResponseEntity<>("Failed to Update", HttpStatus.BAD_REQUEST);
+        }
+
+        if(product1 != null)
+            return new ResponseEntity<>("updated", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Failed to Update", HttpStatus.BAD_REQUEST);
+
+    }
+
+    @DeleteMapping("product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product = service.getProductById(id);
+
+        if(product != null){
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("Product Not Found", HttpStatus.NOT_FOUND);
+        }
     }
 }
