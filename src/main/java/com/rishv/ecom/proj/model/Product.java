5b18d84b577f1dfb51b127c65e1af9a8fe6ecba4
Rishabh Verma <thisisrishv@gmail.com>
2026-03-27 00:28:04 +0530

    fixed post functionality

--- src/main/java/com/rishv/ecom/proj/model/Product.java
+++ src/main/java/com/rishv/ecom/proj/model/Product.java
@@ -16,7 +16,7 @@ import java.util.Date;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String description;
     private String brand;
